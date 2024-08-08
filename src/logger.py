2b8280047124ff39113067a708bffbebdcde833a
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-08-08 09:26:17 +0100

    fixing logger

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,3 @@
-import sys
 from typing import Any
 
 
@@ -6,15 +5,17 @@ class Logger:
     def __init__(self, function) -> None:
         self.function = function
 
-    def __call__(self, output: Any | Exception) -> None:
-        if isinstance(output, Exception):
-            with open("crash_log.txt", "a") as sys.stderr:
-                self.function(output)
-                print()
-        else:
-            with open("log.txt", "a") as sys.stdout:
-                self.function(output)
-                print()
+    def __call__(self, *args, **kwargs) -> None:
+        try:
+            result = self.function(*args, **kwargs)
+            self._log_output(result, "log.txt")
+        except Exception as e:
+            self._log_output(e, "crash_log.txt")
+            raise
+
+    def _log_output(self, output: Any, filename: str) -> None:
+        with open(filename, "a") as log_file:
+            log_file.write(str(output) + "\n")
 
 
 @Logger
@@ -23,25 +24,41 @@ def log_exception(exception: Exception) -> None:
 
 
 @Logger
-def log_string(string: str) -> None:
-    print(string)
+def log_string(string: str) -> str:
+    return string
 
 
 @Logger
-def log_iterable(iterable: list | tuple, name: str = "Array") -> None:
-    print(name + ":")
+def log_iterable(iterable: list | tuple, name: str = "Array") -> str:
+    string = name + ":\n"
     for i in iterable:
-        print("/t" + i)
+        string += "\t" + str(i)
+    return string + "\n"
 
 
 @Logger
-def log_dict(json: dict, name: str = "json") -> None:
-    print(name + ":")
-    from pprint import pp
+def log_dict(json: dict, name: str = "json") -> str:
+    def pprint_json(json: dict | list) -> str:
+        if isinstance(json, dict):
+            string = "{\n"
+            for key, value in json:
+                if not isinstance(value, dict):
+                    string += f"\t{key}: {value}\n"
+                else:
+                    string += f"\t{key}: {pprint_json(value)}\n"
+            string += "}"
+        elif isinstance(json, list):
+            string = "[\n"
+            for i in json:
+                string += f"\t{i},\n"
+            string = "]"
+        return string
 
-    pp(json)
+    string = name + ":\n"
+    string += pprint_json(json=json)
+    return string
 
 
 @Logger
-def log_object(obj: object) -> None:
-    print(obj)
+def log_object(obj: object) -> str:
+    return str(obj)
